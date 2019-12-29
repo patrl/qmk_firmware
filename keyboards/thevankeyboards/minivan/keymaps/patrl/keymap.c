37dc33c138314e1fbddf007a08b457eac43398b1
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-12-29 13:50:19 -0500

    update layout

--- keyboards/thevankeyboards/minivan/keymaps/patrl/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/patrl/keymap.c
@@ -19,6 +19,8 @@ extern keymap_config_t keymap_config;
 #define L_CURBR LSFT(KC_LBRC)
 #define R_CURBR LSFT(KC_RBRC)
 
+#define L_L1 LT(_L1, KC_TAB)
+
 enum custom_keycodes {
   DVORAK = SAFE_RANGE,
   QWERTY,
@@ -28,7 +30,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    MO(_L1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L1),
+    L_L1,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L1),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   MO(_L2),
     KC_LCTL, MO(_L2), KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_LEFT, KC_DOWN, KC_RIGHT
   ),
